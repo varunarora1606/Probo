bfe43aef39deb079dcc577a8be44bf571abce8da
Varun Arora <aroravaruncode@gmail.com>
2025-03-17 23:50:26 +0530

    learn go

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World!!")
+	var name = "Varun"
+	var arr []string
+	fmt.Printf("Hello World %s!!\n", name)
+	arr = append( arr, name)
+	for _, booking := range arr {
+		fmt.Println((booking))
+	}
 }
