5f4ccba1c400d502a7adfb8da96c13beb779a176
Varun Arora <aroravaruncode@gmail.com>
2025-03-29 23:51:34 +0530

    add gracefull shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/varunarora1606/Booking-App-Go/internal/config"
 )
@@ -22,14 +28,34 @@ func main() {
 
 	// Setup server
 	server := http.Server{
-		Addr: cfg.Address,
+		Addr:    cfg.Address,
 		Handler: router,
 	}
 
-	fmt.Println("Server started")
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal("Failed to start server")
-	}
+	slog.Info("Server started on", slog.String("Address", cfg.Address))
 
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		fmt.Println("Server started")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed{
+			log.Fatal("Failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Received shutdown signal, shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Error shutting down server", slog.String("error", err.Error()))
+	} else {
+		slog.Info("Server shutdown successfully")
+	}
 
 }
