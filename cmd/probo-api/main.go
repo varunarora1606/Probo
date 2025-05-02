24f05b8918b08ae4a5f94e8772fe7155c530b903
Varun Arora <aroravaruncode@gmail.com>
2025-05-03 00:46:28 +0530

    bug fix

--- cmd/probo-api/main.go
+++ cmd/probo-api/main.go
@@ -83,7 +83,7 @@ func main() {
 
 	slog.Info("Received shutdown signal, shutting down...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	if err := server.Shutdown(ctx); err != nil {
